2fa98b17c821dc14bd24777368b80c04b2246b05
Scott G. Daniel <scottdaniel25@gmail.com>
2017-12-05 14:16:23 -0700

    new branch

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # download-dbgap
 Just simple script(s) to download SRA data from dbgap
+
+*New branch to make for general SRA files*
+
--- scripts/config.sh
+++ scripts/config.sh
@@ -1,18 +1,16 @@
 export CWD=$PWD
 export SCRIPT_DIR=$CWD
 #root project dir
-export PRJ_DIR="/rsgrps/bhurwitz/scottdaniel/download-dbgap"
+export PRJ_DIR="/rsgrps/bhurwitz/scottdaniel/download-sra"
 #where the worker scripts are (PBS batch scripts and their python/perl workdogs)
 export WORKER_DIR="$SCRIPT_DIR/workers"
 
-#encryption key, change to whatever yours is
-export KEY="prj_9499.ngc"
-
 #cart downloaded from dbgap "run browser" container list of files to download
-export CART="cart_prj9499_phs000790.krt"
+export CART="PRJNA397886_SRR_Acc_List.txt"
 
 #main download / working directory
-export SRA_DIR="/rsgrps/bhurwitz/hurwitzlab/data/raw/Doetschman_20111007/human/phs000790"
+export SRA_DIR="/rsgrps/bhurwitz/hurwitzlab/data/BioProject397886"
+
 # --------------------------------------------------
 function init_dir {
     for dir in $*; do
